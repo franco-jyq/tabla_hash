be7cab3f3e784e97557978b03bb01ba41d3d5d6e
franco <fgazzola@fi.uba.ar>
2020-06-15 21:32:26 -0300

    cambiamos un toq

--- hash.c
+++ hash.c
@@ -1,7 +1,22 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct elementos{
+	void* dato;
+	char* clave;
+};
+
 
 struct hash{
-    
+    lista_t** lista;
+    size_t cantidad;
+    size_t tam;
+    void* puntero_a_funcion;
+    hash_destruir_dato_t destruir_dato;
 }
+
+
 struct hash_iter;
+
